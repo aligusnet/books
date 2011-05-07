d3aecaaca70968fd4d05fdd8362d59ff79db8884
Alexander Ignatyev <ialexander@iMac-Alexander-Ignatyev.local>
2011-05-07 17:24:21 +0400

    added interactive search

--- search.py
+++ search.py
@@ -23,12 +23,15 @@ def search(keywords, idx):
 	
 	def size_cmp(lhs, rhs):
 		return len(lhs) - len(rhs)
-		
+	
 	keywords = lexems.get(keywords)
 	docs = []
 	for keyword in keywords:
 		docs.append(idx.docids(keyword.lower()))
-			
+	
+	if len(docs) == 0:
+		return docs
+	
 	docs.sort(size_cmp)
 	
 	docids = docs[0]
@@ -40,21 +43,33 @@ def search(keywords, idx):
 		result.append(idx.document(docid))
 		
 	return result
-	
-if __name__ == '__main__':
-	if len(sys.argv) < 3:
-		print 'Usage: ' + sys.argv[0] + ' dir keywords'
-		os._exit(os.EX_USAGE)
 
-
-	idx = index.Index(sys.argv[1].decode('utf-8'))
-	
-	keywords = []
-	for i in xrange(2, len(sys.argv)):
-		keywords.append(sys.argv[i].decode('utf-8'))
-		
-	docs = search(' '.join(keywords), idx)
+def printResult(docs):
 	for file_name in docs:
 		info = epub.get_info(file_name)
 		print info.author.strip() + '.', info.title + '. (' + file_name + ')'
 
+if __name__ == '__main__':
+	if len(sys.argv) < 2:
+		print 'Usage: ' + sys.argv[0] + ' dir [keywords]'
+		os._exit(os.EX_USAGE)
+	
+	idx = index.Index(sys.argv[1].decode('utf-8'))
+	
+	if len(sys.argv) > 2:
+		keywords = []
+		for i in xrange(2, len(sys.argv)):
+			keywords.append(sys.argv[i].decode('utf-8'))
+		docs = search(' '.join(keywords), idx)
+		printResult(docs)
+		os._exit(os.EX_OK)
+		
+	print 'press Ctrl-D to exit'
+	while True:
+		try:
+			keywords = raw_input('# ').decode('utf-8')
+		except EOFError:
+			print ('')
+			break
+		docs = search(keywords, idx)
+		printResult(docs)
